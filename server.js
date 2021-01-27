24041c3c8945dc251d5d5b6a10db5ecba6eb4d1e
dkristof99 <kd648@live.mdx.ac.uk>
2021-01-27 23:39:22 +0000

    commit 2

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express()
+
+const MongoClient = require("mongodb").MongoClient;
+let db;
+MongoClient.connect("mongodb+srv://dkristof:1234@cluster0.wkaio.mongodb.net/webstore?retryWrites=true&w=majority", (err, client) => {
+    db = client.db("lesson")
+})
+
+app.use(express.json())
+
+//Get the MongoDB collection name
+app.param("collectionName", (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
+app.get("/", (req, res, next) => {
+    res.send("Select a collection, e.g., /collection/:collectionName")
+})
+
+app.get("/collection/:collectionName", (req, res, next) => {
+    req.collection.find({}, {limit: 5, sort: [['price', -1]]}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    })
+})
+
+app.get("/collection/", (req, res, next) => {
+    res.send("Select a collection, e.g., /collection/lessons")
+})
+
+app.listen(3000, ()=> {
+    console.log("Express server is running at localhost:3000.")
+})
+
+//Get an object based ID
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.findOne(
+        { _id: new ObjectID(req.params.id) },
+        (e, result) => {
+            if (e) return next(e)
+            res.send(result)
+        }
+    )
+})
+
+//Add a new object
+app.post('collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
+
