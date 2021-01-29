90c78938f7fa1dea83fdbee825ca93971cabc898
dkristof99 <kd648@live.mdx.ac.uk>
2021-01-29 08:01:02 +0000

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 
 //Allows cross-origin access
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     //Allow all methods
     res.header('Access-Control-Allow-Methods', "*");
     //allow different header fields
